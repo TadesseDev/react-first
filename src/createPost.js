e9790d6baf7dd2b42314811be3340cd5de90ebc7
Tadesse-Alemayehu <email24t@gmail.com>
2021-06-16 13:05:04 +0300

    form fild added

--- src/createPost.js
+++ src/createPost.js
@@ -1,9 +1,38 @@
-const createPost = () => {
+import { useState } from "react";
+
+const CreatePost = () => {
+  const [blogTitle, updateTitle] = useState("simple state");
+  const [blogBody, updateBody] = useState("simple state");
+  const onchangeHandle = (value, state) => {
+    // console.log(value);
+    console.log("form submited");
+  };
+  const formControl = (e) => {
+    e.preventDefault();
+    console.log("form submited");
+  };
   return (
     <div className="createPsot">
-      <h2> this is create page</h2>
+      <div className="formContainer">
+        <h2> fill the form to create one</h2>
+        <form>
+          <label> Blog Title</label>
+          <input
+            type="text"
+            rows="10"
+            onChange={(e, updateTitle) => onchangeHandle}
+          />
+          <label>Blog content</label>
+          <textarea rows="10" value={blogBody} />
+          <div>
+            <button className="readMore" onClick={(e) => formControl(e)}>
+              Add Blog
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
 
-export default createPost;
+export default CreatePost;
