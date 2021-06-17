fe7472db189737eef29e67ad3ad9b4845271e0e6
Tadesse-Alemayehu <email24t@gmail.com>
2021-06-17 17:26:15 +0300

    uri updte http://winmac.epizy.com/database/blog-data.json

--- src/Home.js
+++ src/Home.js
@@ -29,7 +29,7 @@ const Home = () => {
   // const [loading, setLoadingState] = useState(true);
   // const [errore, setErroreState] = useState({ text: "", isTrue: false });
   const [loading, errore, blogPost] = FeatchUpdate({
-    uri: "http://localhost:4000/blogs",
+    uri: "http://winmac.epizy.com/database/blog-data/blogs",
   });
   // window.addEventListener("DOMContentLoaded", () => getPostDAta(whatever));
   // useEffect(() => {
